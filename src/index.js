35582d250b4c8ccddc7a8e7c546c20f8a6b11ac9
Renaat De Muynck <renaat.demuynck@gmail.com>
2019-11-30 11:43:16 +0100

    Get current group category data with groups and users

--- src/index.js
+++ src/index.js
@@ -1,13 +1,45 @@
 
-export default function (app) {
-    app.addRouteListener('courses.users.groups', function (params) {
-        app.api.get(`/courses/${params.courseId}/groups`).then(result => {
-            var blob = new Blob([JSON.stringify(result, null, 2)], { type: 'application/json' });
-            var url = window.URL.createObjectURL(blob);
+function paramsHashHack(params) {
+    var hashMatch = window.location.hash.match(/^#tab-(\d+)/);
+
+    if (hashMatch !== null) {
+        params.groupCategoryId = hashMatch[1];
+    }
+
+    return params;
+}
+
+export default function ({ router, addReadyListener, api }) {
+
+    async function getGroupCategory(groupCategoryId) {
+        var [groupCategory, groups] = await Promise.all([
+            api.get(`/group_categories/${groupCategoryId}`),
+            api.get(`/group_categories/${groupCategoryId}/groups`)
+        ]);
+
+        groupCategory.groups = await Promise.all(groups.map(async group => {
+            group.users = await api.get(`/groups/${group.id}/users`);
+
+            return group;
+        }));
+
+        return groupCategory;
+    }
+
+    router.addListener('courses.users.groups', function (params) {
+        // Hack to get group id from hash
+        params = paramsHashHack(params);
+
+        getGroupCategory(params.groupCategoryId).then(groupCategory => {
+            var data = [JSON.stringify(groupCategory, null, 2)];
+            var fileName = encodeURIComponent(`${groupCategory.name}.json`);
+            var file = new File(data, fileName, { type: 'application/json' });
+            var url = URL.createObjectURL(file);
             var anchor = document.createElement('a');
             anchor.href = url;
-            anchor.download = 'test.json';
+            anchor.download = '';
             anchor.click();
         });
     });
+
 }
