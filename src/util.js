bef0ee8c29c80664cd4ea740b9402c606fbe23f2
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2020-01-15 09:17:57 +0100

    Combine consecutive character replacements

--- src/util.js
+++ src/util.js
@@ -1,6 +1,6 @@
 export function normalizeWorksheetName(name) {
     // Strip forbidden characters
-    name = name.replace(/[\\\/\?\*\[\]]/g, '_');
+    name = name.replace(/[\\\/\?\*\[\]]+/g, '_');
 
     // Limit name to 31 characters
     name = name.replace(/^(.{30}).{2,}/, '$1…');
