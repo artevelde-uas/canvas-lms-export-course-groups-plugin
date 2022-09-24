6d843a5c02894f34940fddf01a5adadd33e2805b
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2022-09-24 23:35:19 +0200

    'per page' setting is now 100 by default

--- src/index.js
+++ src/index.js
@@ -26,12 +26,12 @@ export default function ({
         // Fetch the group category and its groups
         const [groupCategory, groups] = await Promise.all([
             api.get(`/group_categories/${groupCategoryId}`),
-            api.get(`/group_categories/${groupCategoryId}/groups`, { per_page: 100 })
+            api.get(`/group_categories/${groupCategoryId}/groups`)
         ]);
 
         // Fetch the users from all the groups
         groupCategory.groups = await Promise.all(groups.map(async group => {
-            group.users = await api.get(`/groups/${group.id}/users`, { per_page: 100 });
+            group.users = await api.get(`/groups/${group.id}/users`);
 
             return group;
         }));
